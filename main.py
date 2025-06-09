c22a4ce89bd698392576210a7fa39bef000424c9
void-walker-walking <aaditheguy@gmail.com>
2025-06-10 00:20:27 +0530

    Add polling page which will be updated and UI components

--- main.py
+++ main.py
@@ -4,5 +4,4 @@ import os
 app = create_app()
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 1700))
-    app.run(debug=False, host='0.0.0.0', port=port)
+    app.run(debug=True, port=1900) 
--- website/routes.py
+++ website/routes.py
@@ -4,6 +4,9 @@ from flask_login import login_required, current_user
 routes = Blueprint("routes", __name__)
 
 @routes.route("/")
-@login_required
 def home():
+    return render_template("home.html", user=current_user)
+
+@routes.route("/active_poll")
+def active_poll():
     return render_template("home.html", user=current_user)
