edf27ff41c6025f76129f47ebfddce91727f59d3
void-walker-walking <aaditheguy@gmail.com>
2025-06-11 00:59:06 +0530

    Add login to be required

--- website/routes.py
+++ website/routes.py
@@ -4,9 +4,11 @@ from flask_login import login_required, current_user
 routes = Blueprint("routes", __name__)
 
 @routes.route("/")
+@login_required
 def home():
     return render_template("home.html", user=current_user)
 
 @routes.route("/active_poll")
+@login_required
 def active_poll():
     return render_template("active_poll.html", user=current_user)
